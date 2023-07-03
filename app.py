73e8b78f21ff1945e62adbf5c1e0a45a8d83eed4
Arapuka <117550587+Arapuka@users.noreply.github.com>
2023-07-03 15:47:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def homepage():
         </head>
         <body>   
             <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <center><h1>Bem vido ao Arapuka móvel</h1></center>
+            <center><h1>Bem vindo ao Arapuka móvel</h1></center>
             <br/><br/><br/><br/><br/><br/><br/><br/><br/>
             <center><p>Trabalho de conclusão de curso</p></center>
         </body>
